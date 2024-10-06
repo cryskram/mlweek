9e28f89bf8eb561fe55e9a48b538c5fda1f7c249
Aneesh <aneeshkp1515@gmail.com>
2024-10-06 16:37:57 +0530

    collaborators page fixed

--- app/collaborators/page.tsx
+++ app/collaborators/page.tsx
@@ -5,13 +5,14 @@ const font = Bruno_Ace({ weight: "400", subsets: ["latin"] });
 
 const Collaborators = () => {
   return (
-    <div className="flex flex-col items-center justify-center mt-10">
+    <div className="flex flex-col items-center justify-center mt-10 p-5">
+  
       <h1
-        className={`${font.className} text-5xl bg-gradient-to-r bg-clip-text text-transparent from-fuchsia-400 to-mBlue`}
+        className={`${font.className} text-3xl sm:text-4xl md:text-5xl bg-gradient-to-r bg-clip-text text-transparent from-fuchsia-400 to-mBlue text-center`}
       >
         Collaborators & Sponsors
       </h1>
-      <p className={`${font.className} text-2xl mt-5`}>
+      <p className={`${font.className} text-xl sm:text-2xl mt-5 text-center`}>
         In association with the department of CSE, BMSCE
       </p>
 
@@ -21,6 +22,7 @@ const Collaborators = () => {
         width={500}
         height={500}
         alt="ieee cs bangalore section"
+        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
       />
     </div>
   );
